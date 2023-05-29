6fe782127c022110d5d01da1ac5c2f17a08e50e8
Yash Sachan <yashsachan321@gmail.com>
2023-05-30 00:30:19 +0530

    Added scrollbar section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,11 +5,22 @@ import Exercises from "../components/Exercises";
 import HeroBanner from "../components/HeroBanner";
 import SearchExercises from "../components/SearchExercises";
 const Home = () => {
+  const [exercises, setExercises] = useState([]);
+  const [bodyPart, setBodyPart] = useState("all");
+
   return (
     <Box>
       <HeroBanner></HeroBanner>
-      <SearchExercises></SearchExercises>
-      <Exercises></Exercises>
+      <SearchExercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      ></SearchExercises>
+      <Exercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      ></Exercises>
     </Box>
   );
 };

da0790896809933fd7adb138ada119fdaf5e42d1
Yash Sachan <yashsachan321@gmail.com>
2023-05-30 13:02:33 +0530

    exercise section now working

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,7 +1,37 @@
 import React from "react";
-
-const Exercises = () => {
-  return <div>Exercises</div>;
+import { useState, useEffect } from "react";
+import { Pagination } from "@mui/material";
+import { exerciseOptions, fetchData } from "../utils/fetchData";
+import ExerciseCard from "./ExerciseCard";
+import { Box, Stack, Typography } from "@mui/material";
+const Exercises = ({ exercises, setExercises, bodyPart }) => {
+  console.log(exercises);
+  return (
+    <Box id="#exercises" sx={{ mt: { lg: "110px" } }} mt="50px" p="2-px">
+      <Typography variant="h3" mb="46px">
+        Showing Results
+      </Typography>
+      <Stack
+        direction="row"
+        sx={{ gap: { lg: "110px", xs: "50px" } }}
+        flexWrap="wrap"
+        justifyContent="center"
+      >
+        {exercises.map((exercise, index) => (
+          <ExerciseCard key={index} exercise={exercise}></ExerciseCard>
+        ))}
+      </Stack>
+      <Stack mt="100px" alignItems="center">
+        {exercises.length > 10 && (
+          <Pagination
+            color="standard"
+            shape="rounded"
+            count={Math.ceil(exercises.length / 9)}
+          ></Pagination>
+        )}
+      </Stack>
+    </Box>
+  );
 };
 
 export default Exercises;

bbd80504476cf5bfd99933abc9db68bd5c5bda98
Yash Sachan <yashsachan321@gmail.com>
2023-07-02 00:26:44 +0530

    minor UI improvements

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import { Box } from "@mui/material";
 import "./App.css";
 import ExerciseDetail from "./pages/ExerciseDetail";
+import Lottie from "lottie-react";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
+import Loading from "./assets/loading.json";
 const App = () => {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
   return (
-    <Box width="400px" sx={{ width: { xl: "1488px" } }}>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home></Home>}></Route>
-        <Route
-          path="/exercise/:id"
-          element={<ExerciseDetail></ExerciseDetail>}
-        ></Route>
-      </Routes>
-      <Footer></Footer>
-    </Box>
+    <div className="app">
+      {loading ? (
+        <div className="loading">
+          <Lottie animationData={Loading} className="lottie"></Lottie>
+        </div>
+      ) : (
+        <Box width="400px" sx={{ width: { xl: "1488px" } }}>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home></Home>}></Route>
+            <Route
+              path="/exercise/:id"
+              element={<ExerciseDetail></ExerciseDetail>}
+            ></Route>
+          </Routes>
+          <Footer></Footer>
+        </Box>
+      )}
+    </div>
   );
 };
 
